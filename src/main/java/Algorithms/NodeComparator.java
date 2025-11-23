3600b801d1bee9b93d2b082d90abb61b44cb797d
davessh <david.schlemmer@uabc.edu.mx>
2025-11-23 10:47:20 -0800

    Implementación de un nodo con Comparator que será necesaria para la futura implementación de Dijkstra

---
+++ src/main/java/Algorithms/NodeComparator.java
@@ -0,0 +1,21 @@
+package Algorithms;
+import java.util.Comparator;
+
+//Nodo con compatarator referencia:
+//https://www.geeksforgeeks.org/java/dijkstras-shortest-path-algorithm-in-java-using-priorityqueue/
+public class NodeComparator implements Comparator<NodeComparator> {
+    public int node;
+    public int cost;
+
+    public NodeComparator() {}
+
+    public NodeComparator(int nodo, int costo) {
+        this.node = nodo;
+        this.cost = costo;
+    }
+
+    @Override
+    public int compare(NodeComparator node1, NodeComparator node2) {
+        return Integer.compare(node1.cost, node2.cost);
+    }
+}
